7d1de544eb35fcb9d56134f5e3b7a2cb8ab170e8
Pham Van Truong <pvtruong115@gmail.com>
2018-04-11 13:55:36 +0700

    Update2

--- customer-app/src/app/app.module.ts
+++ customer-app/src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { OrderListComponent } from './orders/order-list/order-list.component';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    OrderListComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+/*
+Copyright 2017-2018 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
