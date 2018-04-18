d813db6caeeee4a5b382d25fcee3fdea2ea992f2
Pham van Truong <30306380+phamtr@users.noreply.github.com>
2018-04-19 06:12:59 +0700

    Update

--- customer-app/src/app/app.module.ts
+++ customer-app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -17,7 +18,8 @@ import { ApiService } from './api.service';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [ApiService],
   bootstrap: [AppComponent]
