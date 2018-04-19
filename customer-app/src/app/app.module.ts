474202168eb8bca7121662fa2b121a0a57479257
Pham Van Truong <pvtruong115@gmail.com>
2018-04-19 11:38:55 +0700

    Update

--- customer-app/src/app/app.module.ts
+++ customer-app/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';  // <-- #1 import module
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ApiService } from './api.service';
+import { AdminService } from './admin.service';
+
 
 @NgModule({
   declarations: [
@@ -19,9 +20,10 @@ import { ApiService } from './api.service';
     FormsModule,
     HttpModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [ApiService],
+  providers: [ApiService, AdminService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
